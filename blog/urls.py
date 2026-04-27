cd2007663316668f1cf4795ea8dd04aebf3b34b3
Евгения Бандукова <zhekabandukova@gmail.com>
2026-04-27 22:33:34 +0300

    Исправления PEP8 pages/views.py

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,9 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
-    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_posts,
+        name='category_posts'
+    ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -43,12 +43,19 @@ posts = [
     },
 ]
 
+
 def index(request):
     return render(request, 'blog/index.html', {'posts': posts})
+
 
 def post_detail(request, id):
     post = posts[id]
     return render(request, 'blog/detail.html', {'post': post})
 
+
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', {'category_slug': category_slug})
+    return render(
+        request,
+        'blog/category.html',
+        {'category_slug': category_slug}
+    )
